a006ba889641bfd6dfaf5d43ec24ca9d9039644b
John Roughley <jroughley@softwareinstitute.com>
2022-08-24 17:01:43 +0100

    modified if/else chain, comments

--- src/solution.cpp
+++ src/solution.cpp
@@ -6,12 +6,13 @@ int Solution::lengthOfLIS(std::vector<int>& nums)
     std::vector<int> cache{nums[0]};  // nums guaranteed to contain at least one value
     for(int n : nums)
     {
-        if(n <= cache.back())  // current value less than current cache maximum --> update the maximum
+        if(n < cache.back())  // current value less than current cache maximum --> update the maximum
         {
             cache.pop_back();  // remove previous maximum
             cache.push_back(n);  // insert new (lower) maximum; cache will always be sorted
         }
-        else{ cache.push_back(n); }  // else value can be appended to cache, increasing size of LIS
+        else if(n == cache.back()){}  // identical value doesn't require a cache operation
+        else{ cache.push_back(n); }  // value can be appended to cache, increasing size of LIS
     }
     return cache.size();  // upon reaching end of nums, cache contains the LIS
 }
