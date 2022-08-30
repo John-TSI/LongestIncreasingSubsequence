a60fab2e81404369b55b5ec31f665c96f74552c6
John Roughley <jroughley@softwareinstitute.com>
2022-08-30 11:40:47 +0100

    fixed the solution using a simpler method

--- src/solution.cpp
+++ src/solution.cpp
@@ -1,18 +1,15 @@
+#include<algorithm>
 #include"../inc/solution.hpp"
 
 
 int Solution::lengthOfLIS(std::vector<int>& nums)
 {
-    std::vector<int> cache{nums[0]};  // nums guaranteed to contain at least one value
+    std::vector<int> seq{nums[0]};  // nums guaranteed to contain at least one value
     for(int n : nums)
     {
-        if(n < cache.back())  // current value less than current cache maximum --> update the maximum
-        {
-            cache.pop_back();  // remove previous maximum
-            cache.push_back(n);  // insert new (lower) maximum; cache will always be sorted
-        }
-        else if(n == cache.back()){}  // identical value doesn't require a cache operation
-        else{ cache.push_back(n); }  // value can be appended to cache, increasing size of LIS
+        if(n > seq.back())
+            seq.push_back(n);  // n larger than largest value in subsequence  --> append to sequence
+        else{ *std::lower_bound(seq.begin(), seq.end(), n) = n; }  // replace smallest value larger than n in seq with n
     }
-    return cache.size();  // upon reaching end of nums, cache contains the LIS
+    return seq.size();  // seq doesn't contain the LIS itself, but will be of the same length
 }
